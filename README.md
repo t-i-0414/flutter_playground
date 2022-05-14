05fd06677a2c2a5b162fe85c4e477f447d09cdd1
t-i-0414 <gutlo.ol0414@gmail.com>
2022-05-15 01:15:37 +0900

    cognito

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my_app
+# helloworld
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,51 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
 
-void main() => runApp(const MyApp());
+import 'amplifyconfiguration.dart';
+import 'resolver.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyChart(),
-    );
-  }
+void main() {
+  runApp(const MyApp());
 }
 
-class MyChart extends StatelessWidget {
-  const MyChart({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+      print('Successfully configured');
+    } on Exception catch (e) {
+      print('Error configuring Amplify: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Code Sample'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(100),
-        child: BarChart(
-          BarChartData(
-              borderData: FlBorderData(
-                  border: const Border(
-                top: BorderSide.none,
-                right: BorderSide.none,
-                left: BorderSide(width: 1),
-                bottom: BorderSide(width: 1),
-              )),
-              groupsSpace: 10,
-              barGroups: [
-                BarChartGroupData(x: 1, barRods: [
-                  BarChartRodData(toY: 10, width: 15),
-                ]),
-                BarChartGroupData(x: 2, barRods: [
-                  BarChartRodData(toY: 9, width: 15),
-                ]),
-                BarChartGroupData(x: 3, barRods: [
-                  BarChartRodData(toY: 4, width: 15),
-                ]),
-                BarChartGroupData(x: 4, barRods: [
-                  BarChartRodData(toY: 2, width: 15),
-                ]),
-                BarChartGroupData(x: 5, barRods: [
-                  BarChartRodData(toY: 13, width: 15),
-                ]),
-                BarChartGroupData(x: 6, barRods: [
-                  BarChartRodData(toY: 17, width: 15),
-                ]),
-                BarChartGroupData(x: 7, barRods: [
-                  BarChartRodData(toY: 19, width: 15),
-                ]),
-                BarChartGroupData(x: 8, barRods: [
-                  BarChartRodData(toY: 21, width: 15),
-                ]),
-              ]),
+    return Authenticator(
+      stringResolver: stringResolver,
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        theme: ThemeData.dark(),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('ホーム')),
+          body: const Center(
+            child: Text('ログイン成功！', style: TextStyle(fontSize: 32.0)),
+          ),
         ),
       ),
     );
